67ecb491c77d8b8d90f75966cbaff9f23f42d470
Viktorija <v.audickaite@gmail.com>
2020-06-02 14:24:29 +0300

    Update images

--- script.js
+++ script.js
@@ -50,9 +50,106 @@ dotsWrap.addEventListener('click', function (event) {
 });
 
 function autoSliding() {
-    setTimeout(function() {
+    setTimeout(function () {
         plusSlides(1);
         autoSliding();
-    }, 4000);
-    
+    }, 6000);
+
+}
+
+// Timer
+//nustatome galinę datą.(deadline)
+let deadline = '2020-06-18';
+
+//Skirtumas tarp dabartinio laiko ir deadlino//
+function getTimeRemaning(endtime) {
+
+    let t = Date.parse(endtime) - Date.parse(new Date());
+
+    seconds = Math.floor((t / 1000) % 60),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        hours = Math.floor(t / 1000 / 60 / 60);
+
+    //Eksportuojam oblektą. Funkcija turi grazinti kazkoki rezultata. Sukuriame nauja objekta
+    return {
+        'total': t,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+    timeInterval = setInterval(updateClock, 1000);
+
+    //funkcija atnaujina laiką kas sekundę.
+
+    function updateClock() {
+        let t = getTimeRemaning(endtime);
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('timer', deadline);
+
+//Counter//
+let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+totalValue.innerHTML = 0;
+
+persons.addEventListener('change', function () {
+    personsSum = +this.value;
+    total = (daysSum + personsSum) * 100;
+
+    if (restDays.value == '') {
+        totalValue.innerHTML = 0;
+    } else {
+        totalValue.innerHTML = total;
+    }
+});
+
+restDays.addEventListener('change', function () {
+    daysSum = +this.value;
+    total = (daysSum + personsSum) * 100;
+
+    if (persons.value == '') {
+        totalValue.innerHTML = 0;
+    } else {
+        totalValue.innerHTML = total;
+    }
+});
+
+place.addEventListener('change', function () {
+
+    if (restDays.value == '' || persons.value == '') {
+        totalValue.innerHTML = 0;
+    } else {
+        let a = total;
+        totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+    }
+
+});
+
+//button-slider///
+
+function scrollIntoId() {
+    document.getElementById('contacts').scrollIntoView({
+        behavior: "smooth"
+    });
 }
